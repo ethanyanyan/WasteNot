312b55a9a1117a0b2fa346606e8bce88ba4ff00f
Ethan Yan <eyyan@wisc.edu>
2025-02-28 17:25:54 -0500

    move around files

--- mobile/WasteNotMobile/WasteNotMobile/Views/Authentication/LoginView.swift
+++ mobile/WasteNotMobile/WasteNotMobile/Views/Authentication/LoginView.swift
@@ -2,47 +2,6 @@
 //  LoginView.swift
 //  WasteNotMobile
 //
-<<<<<<< HEAD
-//  Created by Ethan Yan on 7/2/25.
-//
-
-
-import SwiftUI
-
-struct LoginView: View {
-    // Binding so can update login state
-    @Binding var isAuthenticated: Bool
-
-    @State private var username = ""
-    @State private var password = ""
-
-    var body: some View {
-        VStack(spacing: 20) {
-            Text("WasteNot Login")
-                .font(.largeTitle)
-                .padding(.top, 40)
-
-            // Username Field
-            TextField("Username", text: $username)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 40)
-
-            // Password Field
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 40)
-
-            // Login Button
-            Button("Login") {
-                // Temp
-                isAuthenticated = true
-            }
-            .padding(.top, 20)
-
-            Spacer()
-        }
-        .padding()
-=======
 //  Created by Ethan Yan on 18/1/25.
 //
 
@@ -158,7 +117,6 @@ struct LoginView: View {
         }
         .navigationBarTitle("", displayMode: .inline)
         .navigationBarHidden(true)
->>>>>>> main
     }
 }
 
